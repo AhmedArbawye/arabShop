2ccfa5372966f581c458156f1ae6acc8766164a8
AhmedArabawye <ahmed.arbawye@gmail.com>
2024-04-08 02:48:22 +0300

    add show password by click

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var reg = document.getElementById("reg");
 var regest = document.getElementById("regest");
 var closeReg = document.getElementById("close-reg");
 
+
 reg.onclick = function() {
     regest.style.right = 0;
     closeReg.onclick = function () {
@@ -10,3 +11,9 @@ reg.onclick = function() {
 }
 
 
+var pasd = document.getElementById("pasd");
+
+function show(){
+    pasd.setAttribute("type", "text");
+    pasd.style.paddingLeft = "20px";
+}
